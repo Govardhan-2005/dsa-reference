9a3f066f101a39be4a06993c937c6fd9b47a9d3e
govardhan-2005 <govamanoj6@gmail.com>
2026-09-12 21:47:22 +0530

    added Twosum.java

---
+++ Twosum.java
@@ -0,0 +1,21 @@
+public class Twosum {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == target) {
+
+                    System.out.println("Index: " + i + ", " + j);
+                    System.out.println("Numbers: "
+                            + arr[i] + " + " + arr[j]);
+                }
+            }
+        }
+    }
+}
+
