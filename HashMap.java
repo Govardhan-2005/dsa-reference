8b2d3e0bac62789d1d1393829035cb9c69ba0fc9
govardhan-2005 <govamanoj6@gmail.com>
2026-09-13 22:41:41 +0530

    added HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -1,5 +1,30 @@
-package dsa-reference;
 
 public class HashMap {
-    
+    public static void main(String[] args) {
+
+        String str = "aabbcde";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (map.get(ch) == 1) {
+
+                System.out.println(
+                    "First non-repeating letter: " + ch
+                );
+
+                break;
+            }
+        }
+    }
 }
