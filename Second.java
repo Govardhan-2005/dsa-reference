50cfda2416d9aa89a650c25bab1a497a5448847f
govardhan-2005 <govamanoj6@gmail.com>
2026-09-14 23:02:49 +0530

    added Second.java

--- Second.java
+++ Second.java
@@ -1,3 +1,22 @@
-public class Second {
-    
+public class Second
+ {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 5, 40, 15};
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+
+            if (num > largest) {
+                second = largest;
+                largest = num;
+            } else if (num > second && num != largest) {
+                second = num;
+            }
+        }
+
+        System.out.println("Second Largest: " + second);
+    }
 }
