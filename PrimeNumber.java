5058d7c81bb43178bce6dde1f26d799dfc0820b4
govardhan-2005 <govamanoj6@gmail.com>
2026-09-16 19:31:34 +0530

    added PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,3 +1,25 @@
 public class PrimeNumber {
-    
+    public static void main(String[] args) {
+
+        int num = 17;
+        boolean prime = true;
+
+        if (num <= 1) {
+            prime = false;
+        }
+
+        for (int i = 2; i * i <= num; i++) {
+
+            if (num % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+    }
 }
