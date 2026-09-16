ec9ad5b541e8bd030a6b51a3adb38aaebe7fb275
govardhan-2005 <govamanoj6@gmail.com>
2026-09-16 19:29:06 +0530

    added MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,3 +1,17 @@
 public class MissingNumber {
-    
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 4, 5};
+
+        int n = 5;
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        int total = n * (n + 1) / 2;
+
+        System.out.println("Missing: " + (total - sum));
+    }
 }
